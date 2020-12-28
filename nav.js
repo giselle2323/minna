fe99eb27bb8282860f7f249572ab1d5d4fdcfb5e
giselle2323 <yakubuaminat94@gmail.com>
2020-12-28 17:55:06 +0100

    initial commit: Setup, Hero, About , Stack sections

---
+++ nav.js
@@ -0,0 +1,12 @@
+
+const toggleNav = () => {
+  let menu = document.getElementById("menu-container");
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+
+
+  } else {
+    menu.style.display = "none"
+  }
+}
+
